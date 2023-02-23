24bfc2481f290eb0d978773f47622fda68f721fb
Ruslan <contact@xnrudyson.anonaddy.me>
2023-02-23 23:44:14 +0200

    Created basic models prototypes

---
+++ RadencyLibraryWebAPI/Models/Rating.cs
@@ -0,0 +1,9 @@
+﻿namespace RadencyLibraryWebAPI.Models
+{
+	public class Rating
+	{
+		public int Id { get; set; }
+		public int BookId { get; set; }
+		public decimal Score { get; set; }
+	}
+}
---
+++ RadencyLibraryWebAPI/Models/Review.cs
@@ -0,0 +1,9 @@
+﻿namespace RadencyLibraryWebAPI.Models
+{
+	public class Review
+	{
+		public int Id { get; set; }
+		public int BookId { get; set; }
+		public string? Reviewer { get; set; }
+	}
+}
